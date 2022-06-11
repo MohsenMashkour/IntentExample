eddef2456c0585fc42edb46168884eda24adff54
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2022-06-11 19:36:44 +0200

    second

--- app/src/main/java/com/mkrdeveloper/intentexample/MainActivity.kt
+++ app/src/main/java/com/mkrdeveloper/intentexample/MainActivity.kt
@@ -1,34 +1,32 @@
 package com.mkrdeveloper.intentexample
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btn1 = findViewById<Button>(R.id.button1)
+        val btn2 = findViewById<Button>(R.id.button2)
+
         //explicit Intent
 
-        val btn1 = findViewById<Button>(R.id.button)
-
         btn1.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
+            val intent1 = Intent(this, SecondActivity::class.java)
+            startActivity(intent1)
             finish()
         }
 
-        //implicit Intent
-        val text = "share this text"
 
-        val btn2 = findViewById<Button>(R.id.button2)
+        //implicit Intent
 
         btn2.setOnClickListener {
-
             val intent2 = Intent(Intent.ACTION_SEND)
-            intent2.putExtra(Intent.EXTRA_TEXT, text)
+            intent2.putExtra(Intent.EXTRA_TEXT, "simple text")
             intent2.type = "text/plain"
             startActivity(intent2)
         }
